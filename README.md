8c0ecf2244b57621df77ccb67a4f63d3d11cfd14
joesatrianie <100766267+joesatrianie@users.noreply.github.com>
2023-09-24 09:53:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# test230924
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ve92h9)
---
+++ index.js
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const s= "hello";
+console.log(s.toUpperCase() )
